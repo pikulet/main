8e1fd19bd80c06d8f385c2473bb6f36e48c537fb
teowz46 <teowz46@gmail.com>
2018-10-03 02:31:18 +0800

    expenses: update body of Expense, Expenses, ExpenseType
    This commit updates the attributes and methods of these classes.

--- src/main/java/seedu/address/model/expenses/Expense.java
+++ src/main/java/seedu/address/model/expenses/Expense.java
@@ -1,4 +1,80 @@
 package seedu.address.model.expenses;
 
+import java.util.Date;
+
+/**
+ * Represents a single spending on a single type of service. 
+ */
 public class Expense {
+    
+    private final double cost;
+    private final ExpenseType type;
+    private final Date date;
+
+    /**
+     * Constructs an {@code Expense} object. 
+     * 
+     * @param cost The monetary value of the expense. 
+     * @param type The product or service exchanged for with this expense. 
+     */
+    public Expense(double cost, ExpenseType type) {
+        this.cost = cost;
+        this.type = type;
+        this.date = new Date();
+    }
+
+    /**
+     * Accessor method for the cost. 
+     * 
+     * @return The monetary value of the expense. 
+     */
+    public double getCost() {
+        return cost;
+    }
+
+    /**
+     * Accessor method for the type. 
+     * 
+     * @return The product or service exchanged for with this expense. 
+     */
+    public ExpenseType getType() {
+        return type;
+    }
+
+    /**
+     * Accessor method for the date. 
+     * 
+     * @return The date and time of this transaction. 
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * Accessor method for the date in a string. 
+     * 
+     * @return The date and time of this transaction in string. 
+     */
+    public String getDateString() {
+        return date.toString();
+    }
+    
+    @Override
+    public String toString() {
+        return cost + " spent on " + type + " on " + getDateString();
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Expenses // instanceof handles nulls
+                && cost == ((Expense) other).cost
+                && type == ((Expense) other).type
+                && date.equals(((Expense) other).date)); // state check
+    }
+    
+    @Override
+    public int hashCode() {
+        return date.hashCode();
+    }
 }
--- src/main/java/seedu/address/model/expenses/ExpenseType.java
+++ src/main/java/seedu/address/model/expenses/ExpenseType.java
@@ -1,4 +1,28 @@
 package seedu.address.model.expenses;
 
+/**
+ * Represents all the different types of expenses available at the hotel. 
+ */
 public enum ExpenseType {
+    ROOM_SERVICE, 
+    RESTAURANT, 
+    BAR, 
+    SWIMMING_POOL;
+
+    /**
+     * Checks if a given string is the name of one of the expense types. 
+     * String does not have to be case-sensitive. 
+     * 
+     * @param typeString The string to be tested. 
+     * @return True if the string is the name of a valid expense type, false otherwise. 
+     */
+    public static boolean isValidExpenseType(String typeString) {
+        String upper = typeString.toUpperCase();
+        for (ExpenseType t : ExpenseType.values()) {
+            if (upper.equals(t.toString())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
