185d6a4bbd1b1ce7ade01199b87228c6b0ca8ade
Joyce <jycyeo@yahoo.com.sg>
2018-10-04 02:47:39 +0800

    AddCommand.java: AddCommand accepts -r prefix and parses contents, but does nothing

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ROOM;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Set;
@@ -17,6 +18,7 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Guest;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
+import seedu.address.model.room.RoomNumber;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -31,8 +33,8 @@ public class AddCommandParser implements Parser<AddCommand> {
      */
     public AddCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
-
+            ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE,
+                    PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG, PREFIX_ROOM);
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
@@ -45,6 +47,9 @@ public class AddCommandParser implements Parser<AddCommand> {
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
         Guest guest = new Guest(name, phone, email, address, tagList);
+
+        String userRoomInput = argMultimap.getValue(PREFIX_ROOM).orElse("");
+        RoomNumber roomNumber = ParserUtil.parseRoomNumber(userRoomInput);
 
         return new AddCommand(guest);
     }
---
+++ src/main/java/seedu/address/model/room/RoomNumber.java
@@ -0,0 +1,13 @@
+package seedu.address.model.room;
+
+public class RoomNumber {
+
+    public static final String MESSAGE_ROOM_NUMBER_CONSTRAINTS = "TEST_MESSAGE";
+
+    public RoomNumber(String trimmedRoomNumber) {
+    }
+
+    public static boolean isValidRoomNumber(String trimmedRoomNumber) {
+        return true;
+    }
+}
