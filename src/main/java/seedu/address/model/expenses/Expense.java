831cd8e89356cb80deaa8bf2c45331501a02c5c5
teowz46 <teowz46@gmail.com>
2018-10-03 11:07:13 +0800

    expense: strip trailing whitespace

--- src/main/java/seedu/address/model/expenses/Expense.java
+++ src/main/java/seedu/address/model/expenses/Expense.java
@@ -3,19 +3,19 @@ package seedu.address.model.expenses;
 import java.util.Date;
 
 /**
- * Represents a single spending on a single type of service. 
+ * Represents a single spending on a single type of service.
  */
 public class Expense {
-    
+
     private final double cost;
     private final ExpenseType type;
     private final Date date;
 
     /**
-     * Constructs an {@code Expense} object. 
-     * 
-     * @param cost The monetary value of the expense. 
-     * @param type The product or service exchanged for with this expense. 
+     * Constructs an {@code Expense} object.
+     *
+     * @param cost The monetary value of the expense.
+     * @param type The product or service exchanged for with this expense.
      */
     public Expense(double cost, ExpenseType type) {
         this.cost = cost;
@@ -24,46 +24,46 @@ public class Expense {
     }
 
     /**
-     * Accessor method for the cost. 
-     * 
-     * @return The monetary value of the expense. 
+     * Accessor method for the cost.
+     *
+     * @return The monetary value of the expense.
      */
     public double getCost() {
         return cost;
     }
 
     /**
-     * Accessor method for the type. 
-     * 
-     * @return The product or service exchanged for with this expense. 
+     * Accessor method for the type.
+     *
+     * @return The product or service exchanged for with this expense.
      */
     public ExpenseType getType() {
         return type;
     }
 
     /**
-     * Accessor method for the date. 
-     * 
-     * @return The date and time of this transaction. 
+     * Accessor method for the date.
+     *
+     * @return The date and time of this transaction.
      */
     public Date getDate() {
         return date;
     }
 
     /**
-     * Accessor method for the date in a string. 
-     * 
-     * @return The date and time of this transaction in string. 
+     * Accessor method for the date in a string.
+     *
+     * @return The date and time of this transaction in string.
      */
     public String getDateString() {
         return date.toString();
     }
-    
+
     @Override
     public String toString() {
         return cost + " spent on " + type + " on " + getDateString();
     }
-    
+
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
@@ -72,7 +72,7 @@ public class Expense {
                 && type == ((Expense) other).type
                 && date.equals(((Expense) other).date)); // state check
     }
-    
+
     @Override
     public int hashCode() {
         return date.hashCode();
--- src/main/java/seedu/address/model/expenses/ExpenseType.java
+++ src/main/java/seedu/address/model/expenses/ExpenseType.java
@@ -1,20 +1,20 @@
 package seedu.address.model.expenses;
 
 /**
- * Represents all the different types of expenses available at the hotel. 
+ * Represents all the different types of expenses available at the hotel.
  */
 public enum ExpenseType {
-    ROOM_SERVICE, 
-    RESTAURANT, 
-    BAR, 
+    ROOM_SERVICE,
+    RESTAURANT,
+    BAR,
     SWIMMING_POOL;
 
     /**
-     * Checks if a given string is the name of one of the expense types. 
-     * String does not have to be case-sensitive. 
-     * 
-     * @param typeString The string to be tested. 
-     * @return True if the string is the name of a valid expense type, false otherwise. 
+     * Checks if a given string is the name of one of the expense types.
+     * String does not have to be case-sensitive.
+     *
+     * @param typeString The string to be tested.
+     * @return True if the string is the name of a valid expense type, false otherwise.
      */
     public static boolean isValidExpenseType(String typeString) {
         String upper = typeString.toUpperCase();
