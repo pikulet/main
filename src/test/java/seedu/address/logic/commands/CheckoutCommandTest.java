3a8dad5aac29aef2f62ad28d279bf87427852f08
adamwth <adamwth@hotmail.com>
2018-10-15 19:05:21 +0800

    CheckoutCommandTest.java: Add undo/redo tests

--- src/test/java/seedu/address/logic/commands/CheckoutCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CheckoutCommandTest.java
@@ -1,18 +1,23 @@
 package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Guest;
 import seedu.address.model.room.RoomNumber;
 import seedu.address.testutil.TypicalBookings;
 import seedu.address.testutil.TypicalRoomNumbers;
@@ -101,13 +106,64 @@ public class CheckoutCommandTest {
     }
 
     @Test
-    public void executeUndoRedo_validIndexUnfilteredList_success() {
-        // to be completed after AddressBook is refactored
+    public void executeUndoRedo_validCheckout_success() throws Exception {
+        RoomNumber roomNumberToCheckout = TypicalRoomNumbers.ROOM_NUMBER_010;
+        CheckoutCommand checkoutCommand = new CheckoutCommand(roomNumberToCheckout);
+
+        String expectedMessage = String.format(CheckoutCommand.MESSAGE_CHECKOUT_ROOM_SUCCESS, roomNumberToCheckout);
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.checkoutRoom(roomNumberToCheckout);
+        expectedModel.commitAddressBook();
+        
+        checkoutCommand.execute(model, commandHistory);
+
+        // undo -> reverts addressbook back to previous state and room's bookings to show all bookings
+        expectedModel.undoAddressBook();
+        assertCommandSuccess(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_SUCCESS, expectedModel);
+
+        // redo -> same first room checked out again
+        expectedModel.redoAddressBook();
+        assertCommandSuccess(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
-    public void executeUndoRedo_invalidIndexUnfilteredList_failure() {
-        // to be completed after AddressBook is refactored
+    public void executeUndoRedo_invalidCheckout_failure() {
+        RoomNumber roomNumberToCheckout = TypicalRoomNumbers.ROOM_NUMBER_020;
+        CheckoutCommand checkoutCommand = new CheckoutCommand(roomNumberToCheckout);
+
+        String expectedMessage = String.format(CheckoutCommand.MESSAGE_NO_ROOM_BOOKING, roomNumberToCheckout);
+
+        assertCommandFailure(checkoutCommand, model, commandHistory, expectedMessage);
+
+        // single address book state in model -> undoCommand and redoCommand fail
+        assertCommandFailure(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_FAILURE);
+        assertCommandFailure(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_FAILURE);
+    }
+    
+    @Test
+    public void executeUndoRedo_validCheckout_correctState() throws Exception {
+        RoomNumber roomNumberToCheckout = TypicalRoomNumbers.ROOM_NUMBER_010;
+        CheckoutCommand checkoutCommand = new CheckoutCommand(roomNumberToCheckout);
+
+        String expectedMessage = String.format(CheckoutCommand.MESSAGE_CHECKOUT_ROOM_SUCCESS, roomNumberToCheckout);
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.checkoutRoom(roomNumberToCheckout);
+        expectedModel.commitAddressBook();
+
+        checkoutCommand.execute(model, commandHistory);
+
+        // undo -> reverts addressbook back to previous state and room's bookings to show all bookings
+        expectedModel.undoAddressBook();
+        assertCommandSuccess(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_SUCCESS, expectedModel);
+        
+        // State should have changed such that it is different before and after checkout
+        assertNotEquals(expectedModel, model);
+
+        // redo -> same first room checked out again
+        expectedModel.redoAddressBook();
+        assertCommandSuccess(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
