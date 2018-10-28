c3ccfa7d7c241a363efeb3d19e29958b275ed0f9
Adam <adamwth@hotmail.com>
2018-10-28 18:43:14 +0800

    ConciergeBuilder: add withMenu

--- src/test/java/seedu/address/testutil/ConciergeBuilder.java
+++ src/test/java/seedu/address/testutil/ConciergeBuilder.java
@@ -1,8 +1,10 @@
 package seedu.address.testutil;
 
 import java.util.List;
+import java.util.Map;
 
 import seedu.address.model.Concierge;
+import seedu.address.model.expenses.ExpenseType;
 import seedu.address.model.guest.Guest;
 import seedu.address.model.room.Room;
 
@@ -32,10 +34,18 @@ public class ConciergeBuilder {
     }
 
     /**
-     * Sets the {@code List<Room>} to the {@code Concierge} that we are building.
+     * Sets the {@code List<Room>} to the room list of the {@code Concierge} that we are building.
      */
     public ConciergeBuilder withRooms(List<Room> roomList) {
         concierge.setRooms(roomList);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Map<String, ExpenseType>} to the menu of the {@code Concierge} that we are building.
+     */
+    public ConciergeBuilder withMenu(Map<String, ExpenseType> menu) {
+        concierge.setMenu(menu);
         return this;
     }
 
