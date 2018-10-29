7302073f7c42a5a549e23f0d7b2fe9d8834cd092
Joyce <jycyeo@yahoo.com.sg>
2018-10-30 03:16:15 +0800

    Fix merge conflict errors (#153)
    * Fix merge conflict errors
    
    * CliSyntax.java: Remove unnecessary prefixes
    
    * FindCommand.java: change PREFIX_GUEST to FLAG_GUEST
    
    * AddCommand.java: Update description

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -24,7 +24,6 @@ import static seedu.address.logic.commands.CommandTestUtil.ROOM_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.ROOM_DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_HUSBAND;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_END_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_END_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_START_AMY;
@@ -147,12 +146,12 @@ public class AddCommandParserTest {
 
         // missing address prefix
         assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB
-                        + EMAIL_DESC_BOB + VALID_ADDRESS_BOB,
+                        + EMAIL_DESC_BOB ,
                 expectedMessage);
 
         // all prefixes missing
         assertParseFailure(parser, VALID_NAME_BOB + VALID_PHONE_BOB
-                        + VALID_EMAIL_BOB + VALID_ADDRESS_BOB,
+                        + VALID_EMAIL_BOB,
                 expectedMessage);
     }
 
